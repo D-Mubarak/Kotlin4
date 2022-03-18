a8600cc8d2e44985c5ba6a69c5a161865edb8060
D-Mubarak <danielmubarakamir@gmail.com>
2022-03-18 21:31:21 +0600

    Initial commit

--- app/src/main/java/com/geektech/kotlin4/ui/fragments/FirstFragment.kt
+++ app/src/main/java/com/geektech/kotlin4/ui/fragments/FirstFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.geektech.kotlin4.MyViewModel
+import com.geektech.kotlin4.ViewModelMy
 import com.geektech.kotlin4.databinding.FragmentFirstBinding
 import com.geektech.kotlin4.ui.BaseFragment
 
 class FirstFragment : Fragment() {
 
-    private lateinit var viewModel: MyViewModel
+    private lateinit var viewModel: ViewModelMy
     private lateinit var binding: FragmentFirstBinding
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +25,7 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity()).get(MyViewModel::class.java)
+        viewModel = ViewModelProvider(requireActivity()).get(ViewModelMy::class.java)
         binding.btnPlus.setOnClickListener {
             viewModel.onIncrementClick()
         }
--- app/src/main/java/com/geektech/kotlin4/ui/fragments/SecondFragment.kt
+++ app/src/main/java/com/geektech/kotlin4/ui/fragments/SecondFragment.kt
@@ -5,29 +5,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.geektech.kotlin4.MyViewModel
+import com.geektech.kotlin4.ViewModelMy
 import com.geektech.kotlin4.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
     private lateinit var binding: FragmentSecondBinding
-    private lateinit var viewModel: MyViewModel
+    private lateinit var viewModel: ViewModelMy
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding = FragmentSecondBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(requireActivity())[ViewModelMy::class.java]
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(requireActivity()).get(MyViewModel::class.java)
-        viewModel.storyLiveData.observe(viewLifecycleOwner){
-            binding.tvOnSecondF.text = it.toString()
+       viewModel.counter.observe(requireActivity()){
+           binding.tvOnSecondF.text = it.toString()
         }
     }
 }
